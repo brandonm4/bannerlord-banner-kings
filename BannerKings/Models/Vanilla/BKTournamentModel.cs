915b8072396117d47e4381fb551deb8895189ad4
Brandon Mathis <brandon@darkspyre.org>
2022-12-17 06:29:25 -0600

    removed BTA merge.  Changed default settings

--- BannerKings/Models/Vanilla/BKTournamentModel.cs
+++ BannerKings/Models/Vanilla/BKTournamentModel.cs
@@ -1,17 +1,34 @@
 ﻿using BannerKings.Managers.Education;
 using BannerKings.Managers.Populations.Tournament;
 using BannerKings.Managers.Skills;
+
+using System;
+using System.Collections.Generic;
+
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
 
 namespace BannerKings.Models.Vanilla
 {
+    public static class BExtensions
+    {
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+    }
+
     public class BKTournamentModel : DefaultTournamentModel
     {
         public override TournamentGame CreateTournament(Town town)
         {
+
+
             if (BannerKingsConfig.Instance.PopulationManager != null)
             {
                 var data = BannerKingsConfig.Instance.PopulationManager.GetPopData(town.Settlement);
@@ -22,9 +39,10 @@ namespace BannerKings.Models.Vanilla
                 }
             }
 
+
             return base.CreateTournament(town);
         }
-
+                
         public override int GetInfluenceReward(Hero winner, Town town)
         {
             var result = base.GetInfluenceReward(winner, town);
@@ -42,7 +60,7 @@ namespace BannerKings.Models.Vanilla
 
         public override int GetRenownReward(Hero winner, Town town)
         {
-            var result =  base.GetRenownReward(winner, town);
+            var result = base.GetRenownReward(winner, town);
             if (winner != null)
             {
                 var education = BannerKingsConfig.Instance.EducationManager.GetHeroEducation(winner);
